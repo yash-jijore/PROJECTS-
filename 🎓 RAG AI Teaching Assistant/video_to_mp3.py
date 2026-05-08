ce22ce46102353ed58a1c03bd2a17e2dbdab5568
Yash Jijore <yashjijore1@gmail.com>
2026-05-08 14:29:04 +0530

    Add files via upload

---
+++ 🎓 RAG AI Teaching Assistant/video_to_mp3.py
@@ -0,0 +1,10 @@
+# Converts the videos to mp3 
+import os 
+import subprocess
+
+files = os.listdir("videos") 
+for file in files: 
+    tutorial_number = file.split(" [")[0].split(" #")[1]
+    file_name = file.split(" ｜ ")[0]
+    print( tutorial_number,  file_name)
+    subprocess.run(["ffmpeg", "-i", f"videos/{file}", f"audios/{tutorial_number}_{file_name}.mp3"])
